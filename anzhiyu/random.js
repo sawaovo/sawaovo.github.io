87cc1e6db144c7b050ccfc990a80f19431a96a42
sawaovo <yan2238264304@qq.com>
2024-01-03 20:11:13 +0800

    Site updated: 2024-01-03 20:11:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/Crazyb-Songs.html","posts/Niki-2023birthday.html","posts/Niki-POWEROFYOU.html","posts/Niki-8THanniversary.html","posts/Niki-idolprojectCN.html","posts/Niki-activity-stories.html","posts/Niki-profile.html","posts/Niki-main-stories.html","posts/Niki-resource.html","posts/Niki-relation.html","posts/Niki-timeline.html","posts/Niki-scout-stories.html"];function toRandomPost(){
+var posts=["posts/Crazyb-Songs.html","posts/Niki-2023birthday.html","posts/Niki-8THanniversary.html","posts/Niki-POWEROFYOU.html","posts/Niki-activity-stories.html","posts/Niki-idolprojectCN.html","posts/Niki-main-stories.html","posts/Niki-profile.html","posts/Niki-relation.html","posts/Niki-scout-stories.html","posts/Niki-resource.html","posts/Niki-timeline.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
